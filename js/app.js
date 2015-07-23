9b40cb9f65bd09ad8b3a0af79588919ccf4e3afb
azitowolf <azito.wolf3@gmail.com>
2015-07-23 09:31:36 -0400

    saved token to local storage to begin creating persisting sessions

--- js/app.js
+++ js/app.js
@@ -1,53 +1,31 @@
 var currentToken;
 
-$(document).ready(function($) {
-
+function init(){
   //populate all Primary texts and their Suggested texts
   getPtexts();
+  //AJAX for creating user token on login
+  $('#loginBtn').on('click', login);
+  //AJAX for registering a new user
+  $('#register').on('click', register);
 
-  // hide and show learn more
+  //Hide and show buttons
   $('#learnmore-btn').click(function(event) {
     event.preventDefault();
     $('.learnmore').toggleClass('hidden');
   });
-
-  //hide and show the Primary text posting form
+  //Primary text posting form
   $('.createPtext').click(function() {
     $('.createtext').toggleClass('hidden');
   });
-
+  //loginModal
   $('.loginNav').on('click', function() {
     $('.login').toggleClass('hidden');
   });
-
-  //prevent document default link action
+  //prevent document default link actions
   $('#ptexts').on('click', 'a', function(event) {
     event.preventDefault();
   });
 
-  //AJAX for creating user token on login
-  $('#loginBtn').on('click', login);
-  //AJAX for registering a new user
-  $('#register').on('click', function() {
-    $.ajax('https://murmuring-wave-7389.herokuapp.com/register', {
-      contentType: "application/json",
-      method: "POST",
-      data: JSON.stringify({
-        credentials: {
-          name: $('#signup-username').val(),
-          image: 'http://strawberry.com',
-          email: $('#signup-email').val(),
-          password: $('#signup-password').val()
-        }
-      }),
-    }).done(function(data, textStatus) {
-      console.log(data);
-      $('.cd-user-modal').removeClass('is-visible');
-      $('#ptexts').empty();
-      $('#user-name').html('');
-      $('#user-name').append('<span class="glyphicon glyphicon-user"></span>' + data['name']);
-    }).fail(function(jqxhr, textStatus, errorThrown) {
-      console.log(textStatus);
-    });
-  });
-}); // end document ready
+
+}
+$(document).ready(function($) {init()});
--- js/auth.js
+++ js/auth.js
@@ -13,12 +13,36 @@ var login = function(){
       method: "POST"
     }).done(function(data) {
       currentToken = data['token'];
+      window.localStorage.setItem("TOKEN", data['token']);
       console.log(data);
       $('.cd-user-modal').removeClass('is-visible');
       $('#ptexts').empty();
       $('#user-name').html('');
       $('#user-name').append('<span class="glyphicon glyphicon-user"></span>' + data['name']);
       getPtexts();
+    }).fail(function(jqxhr, textStatus, errorThrown) {
+      console.log(textStatus);
+    });
+  }
+
+  var register = function() {
+    $.ajax('https://murmuring-wave-7389.herokuapp.com/register', {
+      contentType: "application/json",
+      method: "POST",
+      data: JSON.stringify({
+        credentials: {
+          name: $('#signup-username').val(),
+          image: 'http://strawberry.com',
+          email: $('#signup-email').val(),
+          password: $('#signup-password').val()
+        }
+      }),
+    }).done(function(data, textStatus) {
+      console.log(data);
+      $('.cd-user-modal').removeClass('is-visible');
+      $('#ptexts').empty();
+      $('#user-name').html('');
+      $('#user-name').append('<span class="glyphicon glyphicon-user"></span>' + data['name']);
     }).fail(function(jqxhr, textStatus, errorThrown) {
       console.log(textStatus);
     });
